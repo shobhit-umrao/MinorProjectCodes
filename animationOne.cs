6fe5cb12ed52fb8166c6fba9240ed8896be7847f
Shobhit Umrao <shobhitumrao@gmail.com>
2020-05-16 20:52:02 +0530

    Add files via upload

---
+++ animationOne.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class animationOne : MonoBehaviour, IVirtualButtonEventHandler 
+{
+    public GameObject controllerZoom;
+    public Animator buildingZoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerZoom = GameObject.Find("VirtualButtonZoom");
+        controllerZoom.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        buildingZoom.GetComponent<Animator>();
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour Zoom_virtual_button)
+    {
+        buildingZoom.Play("animation_zoom");
+        Debug.Log("Zoom Button Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour Zoom_virtual_button)
+    {
+        buildingZoom.Play("none");
+        Debug.Log("Zoom Button Released");
+
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
---
+++ animationTwo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class animationTwo : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject controllerRotate;
+    public Animator buildingRotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerRotate = GameObject.Find("VirtualButtonRotate");
+        controllerRotate.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        buildingRotate.GetComponent<Animator>();
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour Rotate_virtual_button)
+    {
+        buildingRotate.Play("animation_rotate");
+        Debug.Log("Rotate Button Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour Rotate_virtual_button)
+    {
+        buildingRotate.Play("none");
+        Debug.Log("Rotate Button Released");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
